baa83c8cb616c046776b0e1a0a892b867cf62c04
seanjoerick <mcdetails011@gmail.com>
2024-09-30 18:05:15 +0800

    model updated for add friend request and pending request

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -25,6 +25,18 @@ const userSchema = new mongoose.Schema(
 			type: String,
 			default: "",
 		},
+		friends: [
+			{
+				type: mongoose.Schema.Types.ObjectId,
+				ref: "User",
+			},
+		],
+		pendingRequests: [
+			{
+				type: mongoose.Schema.Types.ObjectId,
+				ref: "User",
+			},
+		],
 	},
 	{ timestamps: true }
 );
