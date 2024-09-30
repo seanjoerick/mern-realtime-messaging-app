e2aeb4a4321823cea2d8f10da4ddfb1fef49a711
seanjoerick <mcdetails011@gmail.com>
2024-09-30 17:31:27 +0800

    connect to the database, add models and setting up the back end

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from 'express'; 
 import dotenv from 'dotenv';
 import authRoutes from './routes/auth.route.js';
+import connectToMongoDB from './db/connectToMongoDb.js';
 
 const app = express();
 dotenv.config();
@@ -11,5 +12,6 @@ app.get('/', (req, res) => res.send('Hello world!'));
 app.use('/api/auth', authRoutes);
 
 app.listen(PORT, () => {
+  connectToMongoDB();
   console.log(`Server is running on port ${PORT}`);
 });
