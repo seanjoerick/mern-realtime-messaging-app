61b3fb856a380e1b78b6ecd7c02029310fa5c80c
seanjoerick <mcdetails011@gmail.com>
2024-10-01 03:22:06 +0800

    login functionality done

--- backend/controller/auth.controller.js
+++ backend/controller/auth.controller.js
@@ -2,23 +2,26 @@ import User from '../models/user.model.js';
 import bcryptjs from 'bcryptjs';
 import generateTokenAndSetCookie from '../utils/generateToken.js';
 
+// Sign up a new user
 export const signup = async (req, res, next) => {
   const { fullName, username, password, gender } = req.body;
 
   try {
-
+    // Check if the user already exists
     const existingUser = await User.findOne({ username });
     if (existingUser) {
       return res.status(400).json({ error: 'User already exists!' });
     }
 
+    // Hash the password
     const salt = bcryptjs.genSaltSync(10);
     const hashedPassword = bcryptjs.hashSync(password, salt);
 
+    // Generate profile picture URL
     const boyProfilePic = `https://avatar.iran.liara.run/public/12?username=${username}`;
     const girlProfilePic = `https://avatar.iran.liara.run/public/84?username=${username}`;
 
-
+    // Create a new user object
     const newUser = new User({
       fullName,
       username,
@@ -45,18 +48,45 @@ export const signup = async (req, res, next) => {
 
   } catch (error) {
     console.error('Error during signup:', error);
-    next(error); 
+    next(error);
   }
 };
 
+// Log in an existing user
+export const login = async (req, res, next) => {
+  const { username, password } = req.body;
 
-  export const login = (req, res) => {
-    console.log('Login');
-    res.send('login-user');
-  };
-  
-  export const logout = (req, res) => {
-    console.log('Logout');
-    res.send('logout-user');
-  };
-  
+  try {
+    // Find user by username
+    const validUser = await User.findOne({ username });
+    const validPassword = bcryptjs.compareSync(password, validUser?.password || '');
+
+    // Check credentials
+    if (!validUser || !validPassword) {
+      return res.status(400).json({ error: 'Wrong credentials!' });
+    }
+
+    // Generate JWT token and set cookie
+    await generateTokenAndSetCookie(validUser._id, res);
+
+    // Send response
+    res.status(200).json({
+      success: true,
+      message: 'User logged in successfully!',
+      _id: validUser._id,
+      fullName: validUser.fullName,
+      gender: validUser.gender,
+      profilePic: validUser.profilePic,
+    });
+
+  } catch (error) {
+    console.log('Error in login controller', error);
+    next(error);
+  }
+};
+
+// Log out user
+export const logout = (req, res) => {
+  console.log('Logout');
+  res.send('logout-user');
+};
