eed536ec27500e0e959ff3ab26532e778e062f78
seanjoerick <mcdetails011@gmail.com>
2024-10-03 19:37:38 +0800

    Chat, Chat area, Messages User interface

--- client/src/components/Chat.jsx
+++ client/src/components/Chat.jsx
@@ -1,23 +1,14 @@
 import React, { useState } from 'react';
 import Sidebar from './Sidebar';
 import ChatArea from './ChatArea';
-import UserSearch from './UserSearch';
 const Chat = () => {
-  const [searchTerm, setSearchTerm] = useState('');
-
-  const handleSearch = (term) => {
-    setSearchTerm(term);
-
-  };
-
+  
   return (
     <div className="flex h-screen antialiased text-gray-800">
       <div className="flex flex-row h-full w-full overflow-x-hidden">
         <Sidebar />
         <div className="flex flex-col flex-auto h-full p-6">
-          {/* Include the UserSearch component */}
-          <UserSearch placeholder="Search for users..." onSearch={handleSearch} />
-          <ChatArea searchTerm={searchTerm} />
+          <ChatArea />
         </div>
       </div>
     </div>
